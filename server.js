5698c7b4780d96b7f726ecf526534a9aec497e24
movik21 <moorvik1@students.zhaw.ch>
2024-05-18 09:55:25 +0200

    2. Änderung vom Node-Beispiel

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('DevOps Deployment Node-Beispiel LÄUFT auf Azure ');
+  res.send('DevOps Deployment Node-Beispiel LÄUFT aus DockerHub im Container auf Azure');
 });
 
 app.listen(PORT, HOST);
