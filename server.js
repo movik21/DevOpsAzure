0d7694ad99dae2dbae7df7ffece19fade04b44e6
movik21 <moorvik1@students.zhaw.ch>
2024-05-18 11:04:28 +0200

    Update für GitHub Actions Start

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('DevOps Deployment Node-Beispiel LÄUFT, aus DockerHub auf GitHub Action (:');
+  res.send('DevOps Deployment Node-Beispiel LÄUFT über GitHub Action und aktualisiert auch DockerHub Image (:');
 });
 
 app.listen(PORT, HOST);
